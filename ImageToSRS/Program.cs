90464e74ce49bffdf110d1b460033cc3f98bfa89
Çağrı Emre Yıldız <yildizcag@itu.edu.tr>
2021-05-23 11:52:47 +0300

    Updated for readability.

--- ImageToSRS/Program.cs
+++ ImageToSRS/Program.cs
@@ -9,8 +9,9 @@ namespace ImageToSRS {
         static void Main(string[] args) {
             var inMemoryList = new List<List<int>>();
             var itemList = new SortedList<int, int>();
-            using (var file = new StreamReader(@"C:\Users\yildizcag\source\repos\ImageToSRS\ImageToSRS\bin\Debug\netcoreapp3.1\temp_to_sharp_yoochoose1_64.csv")) {
-                string line;//= file.ReadLine();
+            //Read data
+            using (var file = new StreamReader(args[0])) {
+                string line;
                 while ((line = file.ReadLine()) != null) {
                     var lineList = new List<int>();
                     var array = line.Split(',');
@@ -27,12 +28,10 @@ namespace ImageToSRS {
                     inMemoryList.Add(lineList);
                 }
             }
+            //Create adjacency matrix
             var masterDict = new SortedList<int, SortedList<int, int?>>();
             foreach (var item in itemList) {
                 masterDict.Add(item.Key, new SortedList<int, int?>());
-                //foreach (var column in itemList) {
-                //    masterDict[item.Key].Add(column.Key, 0);
-                //}
             }
             foreach (var row in masterDict) {
                 foreach (var session in inMemoryList) {
@@ -46,27 +45,20 @@ namespace ImageToSRS {
                     }
                 }
             }
+            //Write matrix to file
             var outputTime = DateTime.Now;
             using (var file = new StreamWriter($"featureSet_full_yoochoose_{outputTime:yyyy-MM-dd_hh_mm_ss}.csv")) {
-                using (var trainingfile = new StreamWriter($"featureSet_training_yoochoose_{outputTime:yyyy-MM-dd_hh_mm_ss}.csv")) {
-                    int counter = 0;
-                    var line = "," + string.Join(',', itemList.Keys);
-                    file.WriteLine(line);
-                    trainingfile.WriteLine(line);
-                    while (masterDict.Count > 0) {
-                        var items = masterDict.First().Value;
-                        foreach (var item in itemList) {
-                            if (!items.ContainsKey(item.Key))
-                                items.Add(item.Key, null);
-                        }
-                        line = masterDict.First().Key.ToString() + "," + string.Join(',', items.Values);
-                        file.WriteLine(line);
-                        if (counter++ % 4 == 0) 
-                        {
-                            trainingfile.WriteLine(line);
-                        }
-                        masterDict.RemoveAt(0);
+                var line = "," + string.Join(',', itemList.Keys);
+                file.WriteLine(line);
+                while (masterDict.Count > 0) {
+                    var items = masterDict.First().Value;
+                    foreach (var item in itemList) {
+                        if (!items.ContainsKey(item.Key))
+                            items.Add(item.Key, null);
                     }
+                    line = masterDict.First().Key.ToString() + "," + string.Join(',', items.Values);
+                    file.WriteLine(line);
+                    masterDict.RemoveAt(0);
                 }
             }
         }
